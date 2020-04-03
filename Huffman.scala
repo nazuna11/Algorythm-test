04809a2cdfafed3042df57aa46b9b9880ac2a9b8
nazuna <nazuna11develop@gmail.com>
2020-04-03 23:51:48 +0900

    [add] appearanceRate(caracter:Char, source: String)

--- Huffman.scala
+++ Huffman.scala
@@ -1,13 +1,14 @@
 object Huffman {
   def main(args: Array[String]): Unit = {
     val test_str = "ABAABACD"
-    val a = 'A'
     val koho = test_str.toCharArray.distinct
-    val a_rate = appearanceRate(a, test_str)
-    koho.foreach(println)
+    val aaa = koho.map(appearanceRate(_, test_str))
+    aaa.foreach(println)
   }
 
   def appearanceRate(caracter:Char, source: String): Double = {
-    return 0.0
+    val charList = source.toCharArray.filter(_ == caracter)
+    val rate = charList.length.asInstanceOf[Double] / source.length
+    return rate
   }
 }
