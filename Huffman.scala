1502d9be5e517c22c136a02ec3c72f5477246dc1
nazuna <nazuna11develop@gmail.com>
2020-04-04 01:01:34 +0900

    [update] set huffman hash

--- Huffman.scala
+++ Huffman.scala
@@ -1,9 +1,14 @@
+import scala.collection.immutable.ListMap
+
 object Huffman {
   def main(args: Array[String]): Unit = {
     val test_str = "ABAABACD"
     val koho = test_str.toCharArray.distinct
     val aaa = koho.map(appearanceRate(_, test_str))
-    aaa.foreach(println)
+
+    val charMap = Map() ++ (koho zip aaa)
+    val sortedMap = ListMap(charMap.toSeq.sortBy(_._1):_*)
+    sortedMap.zipWithIndex.toMap.map(a => (a._1._1 -> a._2.toByte)).foreach(println)
   }
 
   def appearanceRate(caracter:Char, source: String): Double = {
